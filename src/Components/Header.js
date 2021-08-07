ebb52e38215ebdc1d97b84a9f2202359421d5fd3
AndreiDr18 <43674342+AndreiDr18@users.noreply.github.com>
2021-08-08 01:15:01 +0300

    Header + Navbar skeleton

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+const Header = (props) => {
+    const style = {
+        marginLeft:"19vw",
+
+    }
+    return (
+        <div className="header w-100">
+            <a href="www.google.ro" style={style} class="anormalize">aaa</a>
+            <a href="www.google.ro" style={style} class="anormalize">aaa</a>
+            <a href="www.google.ro" style={style} class="anormalize">aaa</a>
+            <a href="www.google.ro" style={style} class="anormalize">aaa</a>
+        </div>
+    )
+    
+}
+
+export default Header;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,34 @@
+function Navbar() {
+    return (
+            <nav className="navbar navbar-expand-lg">
+                <div className="container">
+                    <a className="navbar-brand" href="www.google.ro">BrandName</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon bg-light"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="https://www.google.ro">FAQ</a>
+                            </li> 
+                            <li className="nav-item active">
+                                <a className="nav-link" href="www.google.ro">Community</a>
+                            </li> 
+                            <li className="nav-item active">
+                                <a className="nav-link" href="www.google.ro">About Pitwolf</a>
+                            </li>   
+                            <li className="nav-item active">
+                                <a className="nav-link nav-login" href="www.google.ro">Login</a>
+                            </li>       
+                            <li className="nav-item active">
+                                <a className="nav-link nav-register" href="www.google.ro">Register now</a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+    )
+}
+
+export default Navbar
